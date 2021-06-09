9f0f8eb5325a676bfdaef98344ecae2fd361f201
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-06-09 11:32:19 -0600

    commit tree 6dc6e8b940709432e34de2abd4abd6b8af984934

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,38 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = data[season].find do |contestant|
+    contestant["status"] == "Winner"
+  end
+  winner_name = winner["name"].split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  matching_contestant = data.values.flatten.find do |contestant|
+    contestant["occupation"] == occupation
+  end["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.reduce(0) do |count, contestant|
+    if contestant["hometown"] == hometown
+      count + 1
+    else 
+      count
+    end
+  end
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find do |contestant|
+    contestant["hometown"] == hometown
+  end["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_ages = data[season].sum do |contestant|
+    contestant["age"].to_f
+  end
+  total_contestants = data[season].length
+  average = (total_ages/total_contestants).round
 end
